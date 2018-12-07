1dba37d582997df73e103c72765a456dd6a42d06
Kamil Zdrojewski <kamil.zdrojew@gmail.com>
2018-12-07 11:55:04 +0100

    no message

---
+++ projekt/src/main/java/pl/locon/project/Main.java
@@ -0,0 +1,43 @@
+package pl.locon.project;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            String file = "C:\\Users\\Kamil\\Desktop\\test.xlsx";
+
+            FileInputStream test = new FileInputStream(new File(file));
+            Workbook workbook = new XSSFWorkbook(test);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = datatypeSheet.iterator();
+
+            while (iterator.hasNext()) {
+
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + " ");
+                    }
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
